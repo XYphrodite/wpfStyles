9674e3b7bc6e766da823b3f8fb929d7589874cf9
XYphrodite <72796194+XYphrodite@users.noreply.github.com>
2022-05-30 22:41:30 +0300

    Добавьте файлы проекта.

---
+++ Labs_OOP11/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Labs_OOP11.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labs_OOP11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<ToDoModel> _toDoData;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _toDoData = new BindingList<ToDoModel>()
+             {
+                 new ToDoModel(){Text="Доделать лабораторную с wpf",IsDone=true,Priority="High"},
+                 new ToDoModel() { Text = "Почистить зубы",IsDone=true,Priority="Normal" }
+            };
+            choresList.ItemsSource = _toDoData;
+        }
+    }
+}
---
+++ Labs_OOP11/Models/ToDoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs_OOP11.Models
+{
+    internal class ToDoModel
+    {
+        
+        private bool _isDone;
+        private string _priority;
+        private string _text;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+        public string Priority
+        {
+            get { return _priority; }
+            set { _priority = value; }
+        }
+    }
+}
