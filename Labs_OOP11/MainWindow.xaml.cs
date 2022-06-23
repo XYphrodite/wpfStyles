62840393906c4c04fd65c3f450a60d566fd7355a
Maksim <72796194+XYphrodite@users.noreply.github.com>
2022-06-24 02:00:03 +0300

    Update MainWindow.xaml.cs

--- Labs_OOP11/MainWindow.xaml.cs
+++ Labs_OOP11/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
-﻿using Labs_OOP11.Models;
-using System;
-using System.Collections.Generic;
+using Labs_OOP11.Models;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Labs_OOP11
 {
@@ -37,6 +24,11 @@ namespace Labs_OOP11
                  new ToDoModel() { Text = "Почистить зубы",IsDone=true,Priority="Normal" }
             };
             choresList.ItemsSource = _toDoData;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _toDoData.Clear();
         }
     }
 }
